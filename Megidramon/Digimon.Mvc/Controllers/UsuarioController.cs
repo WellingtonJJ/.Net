03b96146e358acf49e81c96cdbf9457a28e93460
jhondonavan <jhondonavan@gmail.com>
2015-11-18 01:37:15 -0200

    Atualização codigo permissionamento controller usuario

--- Megidramon/Digimon.Mvc/Controllers/UsuarioController.cs
+++ Megidramon/Digimon.Mvc/Controllers/UsuarioController.cs
@@ -15,11 +15,52 @@ namespace Digimon.Mvc.Controllers
 
         public ActionResult ui012cadastrarusuario()
         {
+
+            /*
+            var idusuario = Request.Cookies["userId"].Value;
+            var permissao = Request.Cookies["permissao"].Value;
+
+            if (string.IsNullOrEmpty(idusuario))
+            {
+                Response.Redirect("http://www.projetodigimon.com.br/ui002login.jsp");
+            }
+
+            if (!String.IsNullOrEmpty(permissao))
+            {
+                if ((permissao != "3") && (permissao != "1") && (permissao != "2") && (permissao != "4") && (permissao != "6"))
+                {
+                    Response.Redirect("http://www.projetodigimon.com.br/pagina-de-redirecionamento.jsp");
+                }
+            }
+            */
+
             return View();
         }
         [HttpPost]
         public ActionResult ui012cadastrarusuario(Usuario usuario)
         {
+
+                /*
+               var idusuario = Request.Cookies["userId"].Value;
+               var permissao = Request.Cookies["permissao"].Value;
+
+               if (string.IsNullOrEmpty(idusuario))
+               {
+                   Response.Redirect("http://www.projetodigimon.com.br/ui002login.jsp");
+               }
+
+               if (!String.IsNullOrEmpty(permissao))
+               {
+                   if ((permissao != "3") && (permissao != "1") && (permissao != "2") && (permissao != "4") && (permissao != "6"))
+                   {
+                       Response.Redirect("http://www.projetodigimon.com.br/pagina-de-redirecionamento.jsp");
+                   }
+               }
+               */
+
+
+
+
             if (ModelState.IsValid)
             {
                 var appUsuario = new UsuarioAplicacao();
